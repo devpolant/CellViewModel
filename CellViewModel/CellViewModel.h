654e09b3e0a761899f1be5a61ef488c99ff51f0a
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2019-02-02 22:12:53 +0200

    Initial commit

---
+++ CellViewModel/CellViewModel.h
@@ -0,0 +1,19 @@
+//
+//  CellViewModel.h
+//  CellViewModel
+//
+//  Created by Anton Poltoratskyi on 02.02.2019.
+//  Copyright © 2019 Anton Poltoratskyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CellViewModel.
+FOUNDATION_EXPORT double CellViewModelVersionNumber;
+
+//! Project version string for CellViewModel.
+FOUNDATION_EXPORT const unsigned char CellViewModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CellViewModel/PublicHeader.h>
+
+
